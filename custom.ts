9dea6233bcd9f110a9939b6ca9188c6252f19bb5
Martin Williams <martinwork@users.noreply.github.com>
2025-11-30 14:08:14 +0000

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,14 @@
+/**
+ * NoDep blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace testNoDep {
+    /**
+     * times 2
+     * @param value number to double
+     */
+    //% block
+    export function times2(value: number): number {
+        return value * 2;
+    }
+}
